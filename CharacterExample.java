544f832138215af36c1c92bce39fe98f81d26354
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-10-10 18:54:51 +0000

    CharacterExample(Tutorial_Of_Basic_java_4)

---
+++ CharacterExample.java
@@ -0,0 +1,7 @@
+public class CharacterExample {
+    public static void main(String[] args) {
+        System.out.println('@');
+        System.out.println('\u0040');
+        System.out.println('\u2588');
+    }
+}
