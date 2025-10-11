6542528d851a41e92c2466e3b196a56a20b2f926
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-10-11 10:15:52 +0000

    Assignment(Tutorial_Of_Basic_java_6)

---
+++ Assignment.java
@@ -0,0 +1,11 @@
+public class Assignment {
+    public static void main(String[] args) {
+        int x;
+
+        x = 24;
+        System.out.println("Original x-value : " + x);
+
+        x = 45;
+        System.out.println("Current x-value  : " + x);
+    }
+}
