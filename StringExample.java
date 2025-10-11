7e79497df65f657e43957e63e093203232dec9ed
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-10-11 09:43:14 +0000

    StringExample(Tutorial_Of_Basic_java_5)

---
+++ StringExample.java
@@ -0,0 +1,6 @@
+public class StringExample {
+    public static void main(String[] args) {
+        System.out.println("\"Adromeda\", he said.");
+        System.out.println("Andi said, \"No.\"");
+    }
+}
