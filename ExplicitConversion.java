0643cfd77e0e75bb91e758b795ea8b6b7abb5eea
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-10-11 17:31:11 +0000

    ExplicitConversion(Tutorial_Of_Basic_java_7)

---
+++ ExplicitConversion.java
@@ -0,0 +1,11 @@
+public class ExplicitConversion {
+    public static void main(String [] args) {
+        float hasilUjian;
+        int hasilBulat;
+
+        hasilUjian = 78.6f;
+        hasilBulat = (int) hasilUjian;
+
+        System.out.println("Hasil Bulat = " + hasilBulat);
+    }
+}
