67a720acb83e5f019cfb8b0cc34cdbaa594cc447
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-10-11 19:03:00 +0000

    InputAge(Tutorial_Of_Basic_java_8)

--- InputAge.java
+++ InputAge.java
@@ -5,7 +5,7 @@ public class InputAge {
         int age;
 
         Scanner kbd = new Scanner(System.in);
-        System.out.println("input your age: ");
+        System.out.print("input your age: ");
         age = kbd.nextInt();
 
         System.out.println("Thank you. " + "Your age next year: " + (age + 1));
