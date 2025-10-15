d8794c442eaaed568a3fdfed6cacebe0dc8d85fb
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-10-15 14:21:21 +0000

    Fraction(Tutorial_Of_Basic_java_14)

---
+++ Fraction.java
@@ -0,0 +1,14 @@
+public class Fraction {
+    public static void main (String [] args) {
+        double price = 123.456;
+
+        System.out.println(price);
+        System.out.printf("%.2f\n", price);
+        System.out.printf("%.2f\n", price);
+        System.out.printf("%.3f\n", price);
+        System.out.printf("%.4f\n", price);
+        System.out.printf("%.5f\n", price);
+        System.out.printf("%.6f\n", price);
+        System.out.printf("%12.6f\n", price);
+    }
+}
