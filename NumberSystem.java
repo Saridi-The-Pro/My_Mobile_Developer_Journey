a853b7046282dd49b5351d0d8801ced66bbc31e1
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-10-15 14:39:38 +0000

    NumberSystem(Tutorial_Of_Basic_java_16)

---
+++ NumberSystem.java
@@ -0,0 +1,10 @@
+public class NumberSystem {
+    public static void main (String [] args) {
+        int num = 241;
+
+        System.out.printf("Decimal    : %d\n", num);
+        System.out.printf("Octal      : %o\n", num);
+        System.out.printf("Hexadecimal: %x\n", num);
+        System.out.printf("Hexadecimal: %X\n", num);
+    }
+}
