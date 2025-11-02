318ceeded24e5e2d8c4a6859d86e405593682d2d
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-11-02 10:37:21 +0000

    TrueFalse(Tutorial_Of_Basic_java_18)

---
+++ TrueFalse.java
@@ -0,0 +1,9 @@
+public class TrueFalse {
+    public static void main (String[] args) {
+        System.out.println("Nilai true dan false");
+        System.out.println("--------------------");
+
+        System.out.println("Nilai true  = " + true );
+        System.out.println("Nilai false = " + false) ;
+    }
+}
