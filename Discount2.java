03ccd1403e9f06e4aa13599a8f84c0010810cde3
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-11-06 11:20:42 +0000

    Discount2(Tutorial_Of_Basic_java_38)

---
+++ Discount2.java
@@ -0,0 +1,13 @@
+// Discount determination
+//     using if-else
+
+import java.util.Scanner;
+
+public class Discount2 {
+    public static void main(String[] args) {
+        System.out.println("Determination of discounts");
+        System.out.println("--------------------------");
+
+        long amountPurchase, amountPayment, discount;
+    }
+}
