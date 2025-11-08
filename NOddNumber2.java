11a81f704b75e6e318c49a2b79896981b1092ffd
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-11-08 18:05:33 +0000

    NOddNumber(Tutorial_Of_Basic_java_56

---
+++ NOddNumber2.java
@@ -0,0 +1,28 @@
+// Presentation of a series of n odd numbers
+//    using do...while
+//    version 2
+
+import java.util.Scanner;
+
+public class NOddNumber2 {
+    public static void main (String[] args) {
+        int number;
+        int n;
+
+        System.out.println("Series of N odd numbers");
+        System.out.println("=======================");
+
+        Scanner kbd = new Scanner(System.in);
+        System.out.print("N = ");
+        n = kbd.nextInt();
+
+        number = 1;
+        do {
+            System.out.print(number + " ");
+
+            number = number + 2;
+        } while (number <= 2 * n - 1);
+
+        System.out.println(); // Move line
+    }
+}
