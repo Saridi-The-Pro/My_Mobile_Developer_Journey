9cff3aa6ccf64f65c5a98c3121a1cd49dd2ac377
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-11-08 17:08:22 +0000

    OneToFive(Tutorial_Of_Basic_java_53

---
+++ OneToFive.java
@@ -0,0 +1,17 @@
+// Presentation of numbers 1 to 5
+//     using do...while
+
+import java.util.Scanner;
+
+public class OneToFive {
+    public static void main (String[] args) {
+        int number;
+
+        number = 1;
+        do {
+            System.out.println(number);
+
+            number = number + 1;
+        } while (number <= 5);
+    }
+}
