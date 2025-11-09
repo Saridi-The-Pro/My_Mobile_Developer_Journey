dd5a06fe6f9160b348e56a77fac35a902b6503cb
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-11-09 12:17:02 +0000

    For5Until1(Tutorial_Of_Basic_java_64)

---
+++ For5Until1.java
@@ -0,0 +1,10 @@
+// Presentation of numbers 5 to 1
+//    using for
+
+public class For5Until1 {
+    public static void main (String[] args) {
+        for (int number = 5; number >= 1; number--) {
+            System.out.println(number);
+        }
+    }
+}
