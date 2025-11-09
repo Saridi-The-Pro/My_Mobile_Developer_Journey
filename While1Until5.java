be923e8187391f9986f708a3a38986324c746772
Saridi <166946325+Saridi-The-Pro@users.noreply.github.com>
2025-11-09 11:11:49 +0000

    While1Until5(Tutorial_Of_Basic_java_58)

---
+++ While1Until5.java
@@ -0,0 +1,15 @@
+// Presentation of numbers 1 to 5
+//    using while
+
+public class While1Until5 {
+    public static void main (String[] args) {
+        int number;
+
+        number = 1;
+        while (number <= 5) {
+            System.out.println(number);
+
+            number = number + 1;
+        }
+    }
+}
